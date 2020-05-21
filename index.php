ec77ecaf4dd43a2723ecdffb7e085549d94508b6
Changming Tan <ctan9@mail.greenriver.edu>
2020-05-21 02:01:38 -0400

    added validation and sticky form

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -7,7 +8,9 @@ error_reporting(E_ALL);
 session_start();
 
 //Require the autoload file
-require_once('vendor/autoload.php');
+require_once("vendor/autoload.php");
+require_once("model/data-layer.php");
+require_once("model/validate.php");
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
@@ -21,22 +24,36 @@ $f3->route('GET /', function () {
 //Survey route
 $f3->route('GET|POST /survey', function ($f3) {
 
-    $options = array("This midterm is easy", "I like midterms", "Today is Monday", "I will get 100/100 on this midterm");
-
     //If the form has been submitted
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         var_dump($_POST);
 
-        //Store the data in the session array
-        $_SESSION['name'] = $_POST['name'];
-        $_SESSION['options'] = $_POST['options'];
+        //Validation
+        if (!validName($_POST['name'])) {
 
-        //Redirect to Summary page
-        $f3->reroute('summary');
+            //Set an error variable in the F3 hive
+            $f3->set('errors["name"]', "Invalid name");
+        }
+        if (!validOptions($_POST['option'])) {
+
+            //Set an error variable in the F3 hive
+            $f3->set('errors["option"]', "Invalid option");
+        }
+        //Data is valid
+        if (empty($f3->get('errors'))) {
+
+            //Store the data in the session array
+            $_SESSION['name'] = $_POST['name'];
+            $_SESSION['option'] = $_POST['option'];
+
+            //Redirect to Order 2 page
+            $f3->reroute('summary');
+        }
     }
 
-    $f3->set('options', $options);
-
+    $f3->set('options', getOptions());
+    $f3->set('name', $_POST['name']);
+    $f3->set('selectedOption', $_POST['option']);
     $view = new Template();
     echo $view->render('views/survey.html');
 
@@ -49,7 +66,6 @@ $f3->route('GET /summary', function () {
 
     session_destroy();
 });
-
 
 //Run fat free
 $f3->run();
